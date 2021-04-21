2c81bc041ef6a3374ea17bd49779e5a3f85c20cd
Alex R <alejandro.herrera@ciitizen.com>
2021-04-20 19:11:13 -0700

    fix: Date bug

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,17 +7,17 @@ export default function IndexPage (props) {
   return (
     <div>
       {allPosts.map((post, i) => {
-        const { title, description, slug, content } = post
-        console.log(post)
+        const { title, description, slug, content, date } = post
 
         return (
           <div className='p-3 mb-3' key={`${i}-${title}`}>
             <div className='bold'>{title}</div>
+            <div>{date}</div>
             <div>{description}</div>
-            <div>{content}</div>
+            {/* <div>{content}</div> */}
 
             <button className='bg-blue-500 px-3 py-2 text-white rounded-sm hover:bg-blue-600 transition'>
-              <Link href={`posts/${slug}`}>{`posts/${slug}`}</Link>
+              <Link href={`posts/${slug}`}>{slug}</Link>
             </button>
           </div>
         )
