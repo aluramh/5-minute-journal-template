6bc8e6b80ccb32795de479561339d00451828d7e
Alex R <alejandro.herrera@ciitizen.com>
2021-05-04 09:51:13 -0700

    Finished data model

--- cms/config.js
+++ cms/config.js
@@ -5,49 +5,178 @@ export default {
     // swap out "git-gateway" with "test-repo" and navigate to localhost:3000/admin
     // to view Netlify CMS locally (you can't make changes or read actual content from Git
     // this way, but it's great to verify how things will look).
-    // name: 'test-repo',
-    name: 'git-gateway',
-    branch: 'main'
+    name: "test-repo",
+    // name: 'git-gateway',
+    branch: "main",
   },
-  media_folder: 'public/img',
-  public_folder: 'img',
+  media_folder: "public/img",
+  public_folder: "img",
   collections: [
     {
-      name: 'blog-posts',
-      label: 'Blog Posts',
-      folder: '_posts',
+      name: "blog-posts",
+      label: "Blog Posts",
+      folder: "_posts",
       create: true,
-      slug: '{{year}}-{{month}}-{{day}}-{{slug}}',
+      slug: "{{year}}-{{month}}-{{day}}-{{slug}}",
       fields: [
         {
-          label: 'Template Key',
-          name: 'templateKey',
-          widget: 'hidden',
-          default: 'blog-post'
+          label: "Template Key",
+          name: "templateKey",
+          widget: "hidden",
+          default: "blog-post",
         },
-        { label: 'Title', name: 'title', widget: 'string' },
+
+        // ANCHOR: - Before the day starts
+
+        // Title for today
         {
-          label: 'Publish Date',
-          name: 'date',
-          widget: 'date',
-          format: 'YYYY-MM-DD'
+          label: "Title for today",
+          name: "title",
+          widget: "string",
+          required: false,
+          hint: "You can add a title to remember this memorable day",
         },
-        { label: 'Description', name: 'description', widget: 'text' },
+
         {
-          label: 'Featured Post',
-          name: 'featuredpost',
-          widget: 'boolean',
-          required: false
+          label: "Before the day starts",
+          name: "before-day",
+          widget: "object",
+          hint: "This are the main things to remember at the start of your day",
+          fields: [
+            // Grateful things
+            {
+              label: "I am grateful for...",
+              name: "i-am-grateful-for",
+              widget: "object",
+              hint:
+                "Things I am grateful for today. There are always at least 3 things to be thankful for every day.",
+              fields: [
+                {
+                  label: "The 1st thing I am grateful for is...",
+                  name: "grateful-thing-1",
+                  widget: "string",
+                },
+                {
+                  label: "The 2nd thing I am grateful for is...",
+                  name: "grateful-thing-2",
+                  widget: "string",
+                },
+                {
+                  label: "Finally, I am also grateful for...",
+                  name: "grateful-thing-3",
+                  widget: "string",
+                },
+              ],
+            },
+
+            // Today would be great if
+            {
+              label: "What would make today great?",
+              name: "today-great",
+              widget: "object",
+              fields: [
+                {
+                  label: "This would make today great...",
+                  name: "today-great-1",
+                  widget: "string",
+                },
+                {
+                  label: "Today would be splendid because...",
+                  name: "today-great-2",
+                  widget: "string",
+                },
+                {
+                  label: "Last but not least...",
+                  name: "today-great-3",
+                  widget: "string",
+                },
+              ],
+            },
+
+            // Daily affirmation
+            {
+              label: "Daily affirmation. I am...",
+              name: "daily-affirmation",
+              widget: "string",
+              hint:
+                "This is the place where you can brag about how amazing you are!",
+            },
+          ],
+        },
+
+        // ANCHOR: - After the day
+
+        {
+          label: "At the end of the day",
+          name: "after-day",
+          widget: "object",
+          hint: "Let's recap how you did this extraordinary day",
+          fields: [
+            // Grateful things
+            {
+              label: "3 amazing things that happened today",
+              name: "amazing-things",
+              widget: "object",
+              hint: "Amazing things I things that happened today",
+              fields: [
+                {
+                  label: "The 1st thing amazing thing was...",
+                  name: "grateful-thing-1",
+                  widget: "string",
+                  default: "Come back later and fill out this field",
+                },
+                {
+                  label: "I was astonished today because...",
+                  name: "grateful-thing-2",
+                  widget: "string",
+                  default: "Come back later and fill out this field",
+                },
+                {
+                  label: "The last astounding thing was...",
+                  name: "grateful-thing-3",
+                  widget: "string",
+                  default: "Come back later and fill out this field",
+                },
+              ],
+            },
+
+            // Today better
+            {
+              label: "How could I have made today better?",
+              name: "today-better",
+              widget: "string",
+              hint: "There is always room to grow and be better",
+              default: "Come back later and fill out this field",
+            },
+          ],
+        },
+
+        // Today's date
+        {
+          label: "Start time",
+          name: "start",
+          widget: "datetime",
+          date_format: "DD-MMM-YYYY",
+          format: "LLL",
+          picker_utc: false,
+        },
+
+        // Optional fields to embellish the post, if desired
+        {
+          label: "Featured Image",
+          name: "featuredimage",
+          widget: "image",
+          required: false,
+          hint:
+            "If you believe there is an image that captures the essence of today, you can add it here",
         },
         {
-          label: 'Featured Image',
-          name: 'featuredimage',
-          widget: 'image',
-          required: false
+          label: "Tags",
+          name: "tags",
+          widget: "list",
+          required: false,
         },
-        { label: 'Body', name: 'body', widget: 'markdown', required: false },
-        { label: 'Tags', name: 'tags', widget: 'list', required: false }
-      ]
-    }
-  ]
-}
+      ],
+    },
+  ],
+};
